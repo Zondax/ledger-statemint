b65dd6bd7cb839f09e816c41de6e34527c41a538
Carlo Sala <carlosalag@protonmail.com>
2022-11-17 11:41:39 +0100

    update to spec 9320

--- app/src/substrate/substrate_types.h
+++ app/src/substrate/substrate_types.h
@@ -81,13 +81,19 @@ typedef struct {
 ////////////////////////
 
 typedef struct {
-    const uint8_t* _ptr;
-} pd_Balance_t;
-
-typedef struct {
     uint64_t _len;
     const uint8_t* _ptr;
 } pd_Bytes_t;
+
+typedef struct {
+    const uint8_t* _ptr;
+} pd_u8_array_20_t;
+
+typedef compactInt_t pd_Compactu128_t;
+
+typedef struct {
+    const uint8_t* _ptr;
+} pd_Balance_t;
 
 typedef struct {
     pd_CallIndex_t callIndex;
@@ -101,8 +107,6 @@ typedef struct {
     uint64_t _lenBuffer;
     uint32_t callTxVersion;
 } pd_VecCall_t;
-
-typedef compactInt_t pd_Compactu128_t;
 
 typedef struct {
     const uint8_t* _ptr;
